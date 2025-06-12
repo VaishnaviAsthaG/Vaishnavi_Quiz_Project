c8af8183f428e39ff72ebfb82f0197c254cfbbe7
Vaishnavii <167325432+VaishnaviAsthaG@users.noreply.github.com>
2025-06-12 23:51:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Vansh_Vaishnavi_Quiz_Project/", // Add this line
   plugins: [react()],
-})
+});
+
